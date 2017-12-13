6f18123f11bfb83b142d5ebf0fe93ec6530d8f93
Dorab Patel <dorab@matchcraft.com>
2017-12-13 14:10:08 -0800

    Pretty much after a lein new. Cahnged README and LICENSE as well.

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject aaapi "0.1.0-SNAPSHOT"
+  :description "A Clojure API wrapping the Amazon Advertising API"
+  :url "http://www.matchcraft.com"
+  :license {:name "MIT License"
+            :url "https://opensource.org/licenses/MIT"}
+  :dependencies [[org.clojure/clojure "1.9.0"]])
---
+++ src/aaapi/core.clj
@@ -0,0 +1,6 @@
+(ns aaapi.core)
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
